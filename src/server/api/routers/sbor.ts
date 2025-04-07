86ffa8233514ef295a2bc640027a5ecaf183dd14
Archx824 <archie_law@ensigninfosecurity.com>
2025-04-08 00:46:23 +0800

    chore: Remove unused import from sbor router

--- src/server/api/routers/sbor.ts
+++ src/server/api/routers/sbor.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
-import { Reorder } from "framer-motion";
 
 export const sborRouter = createTRPCRouter({
   searchRecord: publicProcedure
